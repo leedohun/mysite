91438756e0e033c4b5696d0c399f5fe019e249bf
leedohun <aakim66@naver.com>
2022-04-08 17:57:21 +0900

    Create Cafe24_retrieve_list_product_category_count file

---
+++ Cafe24_retrieve_list_product_category_count.py
@@ -0,0 +1,24 @@
+import requests
+
+from Cafe24_Data import *
+
+def retrieve_Product_Categories_Count(Access_Token):
+  print("============================================================================================")
+  print("============================================================================================")
+  print("==================== Cafe24 Retrieve a list of product categories count ====================")
+  print("============================================================================================")
+  print("============================================================================================")
+
+  requestsURL = "https://" + mall_id + ".cafe24api.com/api/v2/admin/categories/count"
+
+  # Access Token 요청에 필요한 header
+  h = {
+    'Authorization': 'Bearer ' + Access_Token,
+    'Content-Type': 'application/json',
+    'X-Cafe24-Api-Version': cafe24_version
+  }
+
+  # rest API
+  response = requests.request('GET', requestsURL, headers=h)
+
+  return response
